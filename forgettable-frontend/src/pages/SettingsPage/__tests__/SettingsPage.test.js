ba1333b02180877ea1ae956d28d008475321bff1
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-21 23:40:12 +1300

    update snapshot

--- forgettable-frontend/src/pages/SettingsPage/__tests__/SettingsPage.test.js
+++ forgettable-frontend/src/pages/SettingsPage/__tests__/SettingsPage.test.js
@@ -1,12 +1,23 @@
 import {Settings} from '@mui/icons-material';
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
-
+import {AuthContext} from '../../../context/AuthContext';
+import firebase from 'firebase/compat/app';
 
 it('renders Settings page UI with correct hierarchy', () => {
   const renderer = new ShallowRenderer();
-  renderer.render(<Settings />);
-  const result = renderer.getRenderOutput();
 
-  expect(result).toMatchSnapshot();
+  firebase.auth().onAuthStateChanged(async (u) => {
+    if (u) {
+      renderer.render(
+          <AuthContext>
+            <Settings />
+          </AuthContext>,
+      );
+
+      const result = renderer.getRenderOutput();
+
+      expect(result).toMatchSnapshot();
+    }
+  });
 });
